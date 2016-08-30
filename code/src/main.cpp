fc909ee2b4287a1d526b856a6957d2b8224fae4a
David Pye <davidmpye@gmail.com>
2016-08-31 00:03:10 +0100

    Updates for use with online DB syncing

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -3,20 +3,26 @@
  * https://www.github.com/davidmpye/RFID_access_controller
  * Version 1.2 of board h/w.
  * Alistair MacDonald 2015
- * David Pye 2016
- *
+ * David Pye 2016 - davidmpye@gmail.com
+ * GNU GPL v2.0 or later
+ * Needs: https://github.com/alistairuk/MD5_String library
+ *        https://github.com/davidmpye/EDB_FS library
+ *        Adafruit neopixel library
+ *        ESP8266 Core libraries
  */
-
 #include <ESP8266WiFi.h>
 #include <SPI.h>
 #include <MFRC522.h>
-//Uses Alistair MacDonald's MD5 library, available here: https://github.com/alistairuk/MD5_String
-#include <MD5_String.h>
+#include <MD5_String.h>  //  https://github.com/alistairuk/MD5_String
+
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
+#include <DBHandler.h> //Included as part of project tree.
+//Config
 
-//Pin defintions for MFRC522 wiring
+
+//Pin defintions for MFRC522/SPI wiring
 #define RST_PIN	16
 #define SS_PIN	2
 
@@ -25,33 +31,26 @@
 #define LED_PIN 15
 #define I2C_SDA_PIN 4
 #define I2C_SCL_PIN 0
-
-//Config
 //#define LATCH_MODE 0    //The relay will be triggered for ACTIVATE_TIME ms, then automatically reset.
 #define LATCH_MODE 1    //The relay will be triggered and remain 'on' until another RFID is presented to end session
-
-#define ACTIVATE_TIME 3000 //How long the device remains activated (in milliseconds). No effect if LATCH_MODE is 1...
-
-
-MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create the MFRC522 instance
-Adafruit_NeoPixel led = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800); //status LED
-
-//Add in your card RFID hashes here, separated by a \n
-//eg String allowedCards="blahblah\nfeedbeef"
-
-String allowedCards="e4c4e25f7c68060b6684e4fbf0ed0487\ndfbbcfe0920817d6d463110ab0d5fd7c\n";
-
 
 #if !(LATCH_MODE == 0 || LATCH_MODE == 1)
 #error "LATCH_MODE must be 0 or 1"
 #endif
 
-//Helper function to hash the card ID
-String hashID() {
-  return md5_string((char*)mfrc522.uid.uidByte, mfrc522.uid.size);
-}
+#define ACTIVATE_TIME 3000 //How long the device remains activated (in milliseconds). No effect if LATCH_MODE is 1...
 
-//Activate relay
+const char *ssid = "";
+const char *pw = "";
+const char *syncURL = ""; //URL for synchronising database
+
+const char *filePath = "/cardDB";
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create the MFRC522 instance
+Adafruit_NeoPixel led = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800); //status LED
+DBHandler database(filePath, syncURL);
+
+//Activate device
 void activateDevice() {
   Serial.println("Activating device");
   //LED to green
@@ -79,27 +78,37 @@ void activateDevice() {
 #endif
   Serial.println("Deactivating device");
   digitalWrite(RELAY_PIN, LOW);
-
- //LED back to blue
- led.setPixelColor(0,0,0,250);
- led.show();
-
-  //Wait for 1 second otherwise we can end up reactivating immediately if card still present....
+  //LED back to blue
+  led.setPixelColor(0,0,0,250);
+  led.show();
+  //Wait for 2 seconds otherwise we can end up reactivating immediately if card still present....
   delay(2000);
 }
 
+bool connectToWifi(const char *SSID, const char *pw) {
+    if (WiFi.status() == WL_CONNECTED) return true;
+
+    Serial.print("Connecting to wifi ");
+    WiFi.begin(ssid, pw);
+    //Try for 10 seconds to get on to wifi.  If not, we'll have to run unconnected.
+    for (int i=0; i<20; ++i) {
+      delay(500);
+      Serial.print(".");
+      if (WiFi.status() == WL_CONNECTED) return true;
+    }
+    return false;
+}
+
+
 void setup() {
   Serial.begin(115200);
-  delay(250);
-  Serial.println("Init....");
+  delay(100);
+  Serial.println("Starting...");
+
+  connectToWifi(ssid, pw);
 
   //Relay control pin for the FET
   pinMode(RELAY_PIN, OUTPUT);
-
-  // Init the SPI bus + RFID reader
-  SPI.begin();
-  mfrc522.PCD_Init();
-  mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max); //try gain to max
 
   // Set up a yellow LED while we are getting ready
   led.begin();
@@ -107,40 +116,63 @@ void setup() {
   led.setPixelColor(0,255,255,0);
   led.show();
 
+  // Init the SPI bus + RFID reader
+  SPI.begin();
+  mfrc522.PCD_Init();
+  mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max); //Gain to maximum
+
+  //Initialise and if necessary sync the user database.
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Syncing database");
+    database.sync();
+  }
+  else {
+    Serial.println("Running in offline mode, using local database");
+  }
   //LED to blue.
   led.setPixelColor(0,0,0,250);
   led.show();
 }
 
 void loop() {
-
   Serial.println("Ready, listening for card");
   //Keep trying to sense if a card is present.
   while (! mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
     delay(50);
   }
-  //Found a card.
-  //Calculate the MD5 hash of the Card UID field.
-  String cardHash = hashID();
-
+  //Found a card - generate the MD5 hash of the card UID.
+  String cardHash = md5_string((char*)mfrc522.uid.uidByte, mfrc522.uid.size);
   Serial.println("Read card.  Card UID Hash (MD5): ");
   Serial.println(cardHash);
 
-  //LED to yellow while we check local database
-  led.setPixelColor(0,255,255,0);
-  led.show();
-
-  // Is the card in the cache database
-  if (allowedCards.indexOf(cardHash)>=0) {
+  // Is the card in the local database cache?
+  if (database.contains(cardHash.c_str())) {
     Serial.println("Card allowed - activating device");
     activateDevice();
   }
   else {
-    //Access DENIED....
-    Serial.println("Card not allowed - NOT activating device");
-    //Go red for 1 second
-    led.setPixelColor(0,255,0,0);
+    //Card is not in our database
+    //Check our database is up to date, and if it isn't,
+    //sync it, then check again.
+    //LED to yellow while we do this.
+    led.setPixelColor(0,255,255,0);
     led.show();
-    delay(1000);
+
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("Unrecognised card, attempting database sync.");
+      database.sync();
+    }
+    if (database.contains(cardHash.c_str())) {
+      Serial.println("Card allowed - activating device");
+      activateDevice();
+    }
+    else {
+      //This card is NOT allowed access.
+      Serial.println("Card not allowed - NOT activating device");
+      //Go red for 1 second
+      led.setPixelColor(0,255,0,0);
+      led.show();
+      delay(1000);
+    }
   }
 }
