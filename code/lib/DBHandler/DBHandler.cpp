a20d6034c08099fe191193cd2e87ddb2575be532
David Pye <davidmpye@gmail.com>
2016-08-31 00:02:20 +0100

    DBHandling library for sync/search

---
+++ code/lib/DBHandler/DBHandler.cpp
@@ -0,0 +1,152 @@
+#include <DBHandler.h>
+
+DBHandler::DBHandler(const char *filename, const char *url) {
+  filePath = filename;
+  syncURL = url;
+  if ( ! SPIFFS.begin()) {
+    Serial.println("Unable to init SPIFFS - trying format");
+    //Oh dear.  Try formatting it, might be a fresh board.
+    if ( SPIFFS.format()) {
+      Serial.println("Format successful");
+    }
+    else {
+      Serial.println("Format also failed - aborting.");
+      return;
+    }
+  }
+
+  int result = database.open(filePath);
+  if (result == EDB_ERROR) {
+    Serial.print("Unable to open database, attempting to create new one - ");
+    //Might be a new device without a database, try creating instead.
+    result = database.create(filePath, TABLE_SIZE, sizeof(DBRecord) );
+    if (result  == EDB_OK) {
+      Serial.println("OK");
+      database.setDBVersion("DEADBEEF"); //this will force a sync.
+    }
+    else {
+      Serial.println("FAIL");
+    }
+  }
+  else {
+    Serial.println("Database opened, version ");
+    Serial.print(database.DBVersion());
+    Serial.print(", containing ");
+    Serial.print(database.count());
+    Serial.println(" records");
+  }
+}
+
+DBHandler::~DBHandler() {
+
+}
+
+bool DBHandler::sync() {
+  Serial.println("Checking server database version");
+  HTTPClient client;
+  client.begin(syncURL);
+  client.addHeader("If-None-Match", database.DBVersion());
+
+  const char *headerKeys[] = {"ETag",};
+  client.collectHeaders(headerKeys, 1);
+
+  int result = client.GET();
+
+  if (result != 304 && result != 200) {
+    Serial.println("Bogus server response (not 200/304)");
+    return false;
+  }
+
+  if (result == 304) {
+    Serial.println("DB is in sync");
+    return true;
+  }
+
+  String id = client.header("ETag");
+  const char *newDBID = id.c_str();
+
+  if (strlen(newDBID) != 32) {
+    Serial.print("Invalid DB ETag received from remote server - ");
+    Serial.println(newDBID);
+    return false;
+  }
+
+  Serial.print("Server version of database is ");
+  Serial.println(newDBID);
+  EDB_FS tempDB;
+  tempDB.create("/tmpDB", TABLE_SIZE, sizeof(DBRecord));
+  tempDB.setDBVersion(newDBID);
+
+  WiFiClient *ptr = client.getStreamPtr();
+
+  String buf;
+
+  while (ptr->available()) {
+    buf = ptr->readStringUntil('\r');
+    const char *p = buf.c_str();
+    ptr->read();  //eat the \n (server records are \r\n terminated.)
+    if (strlen(p) == sizeof(DBRecord)-1) {
+      tempDB.appendRec((unsigned char*)p);
+    }
+    else {
+      Serial.print("Rejected row ");
+      Serial.println(p);
+    }
+  }
+
+  //Do the delete/rename
+  Serial.println("Substituting databases..");
+  tempDB.close();
+  database.close();
+  SPIFFS.remove(filePath);
+  SPIFFS.rename("/tmpDB", filePath);
+
+  database.open(filePath);
+
+  Serial.print("Database now contains ");
+  Serial.print(database.count());
+  Serial.print(" records - version ");
+  Serial.println(database.DBVersion());
+}
+
+bool DBHandler::inSync() {
+    Serial.print("Local DB Version - ");
+    Serial.println(database.DBVersion());
+    HTTPClient client;
+    client.begin(syncURL);
+    client.addHeader("If-None-Match", database.DBVersion());
+
+    const char *headerKeys[] = {"Etag",};
+    client.collectHeaders(headerKeys, 1);
+
+    int result = client.GET();
+
+    if (result == 304) {
+      Serial.print("Database is up to date - version ");
+      Serial.println(database.DBVersion());
+      return true;
+    }
+    Serial.println("Not up to date (or failed...!");
+    return false;
+}
+
+bool DBHandler::contains(const char *hash) {
+  DBRecord rec;
+  for (int i=0;  i<database.count(); ++i) {
+    EDB_Status result =  database.readRec(i, EDB_REC rec);
+    if (result == EDB_OK) {
+      if (!strcmp(rec.hash, hash)) {
+        Serial.print("Hash ");
+        Serial.print(hash);
+        Serial.print(" found in database, record no ");
+        Serial.println(i);
+        return true;
+      }
+    }
+    else {
+      //Error
+      Serial.print("Error reading record ");
+    }
+  }
+  return false;
+}
---
+++ code/lib/DBHandler/DBHandler.h
@@ -0,0 +1,34 @@
+#ifndef DBHANDLER
+#define DBHANDLER
+
+#include <Arduino.h>
+#include <FS.h>
+#include <EDB_FS.h>
+#include <ESP8266HTTPClient.h>
+
+typedef struct DBRecord {
+  char hash[33];  //hash is 32 chars + terminating \0
+};
+
+#define REMOTE_DELIM_CHAR '\r' //Currently actually \r\n terminated lines....
+
+#define MAX_RECORDS 500
+#define TABLE_SIZE ( sizeof(DBRecord) * MAX_RECORDS )
+
+class DBHandler {
+
+public:
+  DBHandler(const char *, const char *);
+  ~DBHandler();
+  bool inSync();
+  bool sync();
+  bool contains(const char*);
+
+private:
+  const char *filePath;
+  const char *syncURL;
+
+  EDB_FS database;
+};
+
+#endif
