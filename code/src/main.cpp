e09f5cd277cbecf56b4b112d1012839c2bbd9308
David Pye <davidmpye@gmail.com>
2016-09-14 19:20:14 +0100

    Sync with MQTT code

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -17,7 +17,8 @@
 #include <Adafruit_NeoPixel.h>
 
 #include <MD5_String.h>  //  https://github.com/alistairuk/MD5_String
-#include <DBHandler.h> //Included as part of project tree.
+#include <../lib/DBHandler/DBHandler.h> //Included as part of project tree.
+#include <PubSubClient.h> //for MQTT announcements...
 
 //Pin defintions for MFRC522/SPI wiring
 #define RST_PIN	16
@@ -29,25 +30,61 @@
 #define I2C_SDA_PIN 4
 #define I2C_SCL_PIN 0
 
-//#define LATCH_MODE 0    //The relay will be triggered for ACTIVATE_TIME ms, then automatically reset.
-#define LATCH_MODE 1    //The relay will be triggered and remain 'on' until another RFID is presented to end session
+#define MQTT_SUPPORT //Uncomment to enable MQTT publishing for access.
+
+#define LATCH_MODE 0    //The relay will be triggered for ACTIVATE_TIME ms, then automatically reset.
+//#define LATCH_MODE 1    //The relay will be triggered and remain 'on' until another RFID is presented to end session
+#define ACTIVATE_TIME 3000 //LATCH_MODE 0 only - How long the device remains activated (in milliseconds).
 
 #if !(LATCH_MODE == 0 || LATCH_MODE == 1)
 #error "LATCH_MODE must be 0 or 1"
 #endif
 
-#define ACTIVATE_TIME 3000 //How long the device remains activated (in milliseconds). No effect if LATCH_MODE is 1...
-
 //Config
 const char *ssid = "";
 const char *pw = "";
 const char *syncURL = ""; //URL for synchronising database
+const char *logURL = ""; //The log API needs to be improved for multidevices FIXME
+const char *MQTT_Server="172.18.16.25";
+const char *opendoorMQTTTopic = "/status/backdoor/opened";
 
+#ifdef MQTT_SUPPORT
+WiFiClient espClient;
+PubSubClient client(espClient);
+#endif
+
+//Folder to use on SPIFFS
 const char *filePath = "/cardDB";
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create the MFRC522 instance
 Adafruit_NeoPixel led = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800); //status LED
 DBHandler database(filePath, syncURL);
+
+
+// ****************** FUNCTIONS ******************
+
+#ifdef MQTT_SUPPORT
+//Send MQTT broadcast to space's server.
+void sendMQTT(String hash) {
+  //Connect as backdoor to the server.
+  if (!client.connected() ) client.connect("backdoor");
+  if (client.connected()) {
+        // Once connected, publish an announcement...
+        client.publish(opendoorMQTTTopic, hash.c_str());
+    }
+}
+#endif
+
+//Hit the log URL.
+bool logAccess(String hash, bool granted) {
+  HTTPClient client;
+  String fullURL(logURL);
+  client.begin(fullURL + "?card=" + hash);
+  int result = client.GET();
+  client.end();
+  if (result == 200) return true;
+  return false;
+}
 
 //Produces a blue LED if we're online, and a magenta one if we're running from the local cache
 void ledToRestingColor() {
@@ -58,7 +95,7 @@ void ledToRestingColor() {
 
 //Activate device
 void activateDevice() {
-  Serial.println("Activating device");
+  Serial.print("Activating device");
   //LED to green
   led.setPixelColor(0,0,255,0);
   led.show();
@@ -67,14 +104,14 @@ void activateDevice() {
 
 #if LATCH_MODE == 0  //Non-latching, ie for door controller
 
-  Serial.print("Waiting for ");
+  Serial.print(" - Waiting for ");
   Serial.print(ACTIVATE_TIME/1000);
   Serial.println(" seconds");
   delay(ACTIVATE_TIME);
 
 #elif LATCH_MODE == 1 //Latching, remains triggered ie for milling machine controller etc
 
-  Serial.println("Device activated.  Will remain active until card presented");
+  Serial.print(" - Will remain active until card presented");
   //Wait for 2 seconds for the user to remove their card, otherwise we just keep activating/deactivating
   delay(2000);
   //Wait for a card to be presented to end session
@@ -138,6 +175,8 @@ void setup() {
     Serial.println("Running in offline mode, using local database");
   }
   ledToRestingColor();
+  //Initialise MQTT server
+  client.setServer(MQTT_Server,1883);
 }
 
 bool checkCard(String hash) {
@@ -160,7 +199,9 @@ bool checkCard(String hash) {
 }
 
 void loop() {
-  Serial.println("Ready, listening for card");
+  Serial.println("Ready, listening for card - ");
+  Serial.print("free heap: ");
+  Serial.println(ESP.getFreeHeap());
   //Keep trying to sense if a card is present.
   while (! mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
     delay(50);
@@ -174,6 +215,11 @@ void loop() {
   if (validUser) {
     Serial.println("Card allowed - activating device");
     activateDevice();
+
+#ifdef MQTT_SUPPORT
+    sendMQTT(cardHash); //This isnt ideal for devices that stay active, as it wont send the MQTT until logout...
+#endif
+    logAccess(cardHash, true);
   }
   else {
     //This card is NOT allowed access.
@@ -182,6 +228,8 @@ void loop() {
     led.setPixelColor(0,255,0,0);
     led.show();
     delay(1000);
+    //Log the denied access.
+    logAccess(cardHash, false);
     //Back to normal colour.
     ledToRestingColor();
   }
